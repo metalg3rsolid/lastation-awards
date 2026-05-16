a97b67538fa6a62f696543c6ba62b7f2d22e0e5a
metalg3rsolid <ahmedswose984@gmail.com>
2026-05-16 17:21:16 +0200

    Update config.js

--- api/config.js
+++ api/config.js
@@ -27,42 +27,42 @@ export default function handler(req, res) {
     ],
     categories: [
       {
-        id: "presence",
+        id: "bandeur",
         title: "Le plus gros bandeur ou la plus grosse bandeuse",
         description: "Celui ou celle qui bande sur tout le monde, qui crois que Discord c'est pour hwi."
       },
       {
-        id: "charisme",
+        id: "drole",
         title: "Le ou la plus drôle",
         description: "Celui ou celle qui est vraiment super drôle, super fun."
       },
       {
-        id: "drole",
+        id: "genant",
         title: "Le/la plus génant(e)",
         description: "La personne qui est juste trop chelou, grave bizarre, hyper génant"
       },
       {
-        id: "mysterieux",
+        id: "beau",
         title: "Le/la plus beau/belle",
         description: "Celui ou celle qui est juste trop beau, super beau ou super belle, genre magnifique."
       },
       {
-        id: "retour",
+        id: "hypocrite",
         title: "Le plus ou la plus hypocrite ",
         description: "Gros ou grosse hypocrite qui parle derrière le dos des gens."
       },
       {
-        id: "vocal",
+        id: "pute",
         title: "Le/la plus grosse pute du serveur",
         description: "Pas besoin d'expliquer."
       },
       {
-        id: "debats",
+        id: "moche",
         title: "Le/la plus moche du serveur",
         description: "Super moche, super horrible !"
       },
       {
-        id: "style",
+        id: "inutile",
         title: "Le plus inutile",
         description: "Celui qui sert a rien dans le serveur on sait pas pourquoi elle/il est là"
       }
